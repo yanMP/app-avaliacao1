589eb367c1d442458d9c4b672340cd0ac9b5129e
yanMP <yanrafaelyan@gmail.com>
2024-04-04 13:30:53 -0300

    fazendo o tutorial que o professor pediu

--- App.js
+++ App.js
@@ -2,28 +2,82 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Button from './components/button';
 import ImageViewer from './components/imageViewer';
-
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import CircleButton from './components/circuloButton';
+import IconButton from './components/IconButton';
+import EmojiPicker from "./components/EmojiPicker";
 
 
 const PlaceholderImage = require('./sticker-smash-assets/images/background-image.png');
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-       <View style={styles.imageContainer}>
-       <ImageViewer placeholderImageSource={PlaceholderImage} />
-       
-      </View>
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
 
-      <View style={styles.footerContainer}>
-      <Button theme="primary" label="escolha a foto" />
-        <Button label="escolha a foto" />
-        <Button label="use a foto" />
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert('Você não selecionou nenhuma imagem.');
+    }
+  };
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+
+  
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} 
+          selectedImage={selectedImage} />
+          </View>
+      
+      {showAppOptions ? (
+          <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="escolha a foto" onPress={pickImageAsync} />
+          <Button label="use a foto" />
+        </View>
+        )}
+
+<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        {/* A list of emoji component will go here */}
+      </EmojiPicker>
+        <StatusBar style="auto" />
+        
       </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    );
+
+  }
+
+
+
 
 const styles = StyleSheet.create({
   footerContainer: {
@@ -32,14 +86,21 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: '#0B0B61',
+    backgroundColor: '#25292e',
     alignItems: 'center',
   },
   imageContainer: {
     flex: 1,
     paddingTop: 58,
   },
-
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
 });
 
 
--- components/button.js
+++ components/button.js
@@ -1,13 +1,13 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function Button({ label, theme }) {
+export default function Button({ label, theme, onPress }) {
   if (theme === "primary") {
     return (
       <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
         <Pressable
           style={[styles.button, { backgroundColor: "#fff" }]}
-          onPress={() => alert('você pressionou o botão.')}
+          onPress={onPress}
         >
           <FontAwesome
             name="picture-o"
@@ -23,7 +23,7 @@ export default function Button({ label, theme }) {
 
   return (
     <View style={styles.buttonContainer}>
-        <Pressable style={styles.button} onPress={() => alert('você pressionou o botão.')}>
+        <Pressable style={styles.button} onPress={onPress}>
           <Text style={styles.buttonLabel}>{label}</Text>
         </Pressable>
       </View>
